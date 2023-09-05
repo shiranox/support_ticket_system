2e84ffa54a5892fa184518e4c51c720e8273439e
Shiran Levy <shiran@almond-qms.com>
2023-09-05 10:00:31 +0300

    validation errors

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,8 +2,6 @@ class User < ApplicationRecord
     has_secure_password
 
     enum role: [:regular, :manager]
-    validates :email, presence: true, format: { with: URI::MailTo::EMAIL_REGEXP }
-    validates :password, presence: true
-    validates :password_confirmation, presence: true
+    validates :email, format: { with: URI::MailTo::EMAIL_REGEXP }
 
 end
